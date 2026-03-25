9e0f3ad1c14026b9c5a4afe0832e46f8cbcccbd7
Prakash <prkshhhdas@gmail.com>
2026-03-25 20:11:00 +0530

    Stop scanning ttyS* devices; limit detection to ttyUSB* and ttyACM*

--- src/core/scanner.c
+++ src/core/scanner.c
@@ -1,21 +1,21 @@
 /*
  * ComScope - Serial Port Terminal for Embedded Development
  * Copyright (c) 2026 Prakash Das
- * 
+ *
  * This file is part of ComScope.
  * ComScope is free software: you can redistribute it and/or modify
  * it under the terms of the MIT License.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -50,8 +50,8 @@ int scan_ports(char ports[][270], int max_ports)
 
         /* scan only ttyUSB, ttyACM, ttyS*/
         if (strncmp(name, "ttyUSB", 6) != 0 &&
-            strncmp(name, "ttyACM", 6) != 0 &&
-            strncmp(name, "ttyS", 4) != 0)
+            strncmp(name, "ttyACM", 6) != 0) /*&&
+            strncmp(name, "ttyS", 4) != 0)*/
             continue;
 
         char path[270];
@@ -63,9 +63,12 @@ int scan_ports(char ports[][270], int max_ports)
 
         /* Safely copy path to ports array with explicit size */
         size_t path_len = strlen(path);
-        if (path_len < sizeof(ports[count])) {
-            strcpy(ports[count], path);  /* Safe because we checked length */
-        } else {
+        if (path_len < sizeof(ports[count]))
+        {
+            strcpy(ports[count], path); /* Safe because we checked length */
+        }
+        else
+        {
             /* Path too long, skip this port */
             continue;
         }
